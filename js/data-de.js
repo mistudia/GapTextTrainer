5ae3ab4ce6b9d7f9fd105f4aef68fc906b8bb4c1
mistudia <markautsch@aol.com>
2026-07-14 08:54:01 -0600

    unterstrichen p2

--- js/data-de.js
+++ js/data-de.js
@@ -27,208 +27,209 @@ const germanExercise = {
         solutionsShown: "Die Lösungen wurden angezeigt."
     },
 
-    story: [
+  story: [
 
-        {
-            before:"Jeden Morgen ",
-            after:" Anna eine Tasse Kaffee, bevor sie mit der Arbeit beginnt. ",
-            verb:"trinken",
-            answer:"trinkt",
-            signal:"jeden Morgen",
-            tense:"Präsens",
-            formation:"Verb im Präsens"
-        },
+{
+    before:"Jeden Morgen ",
+    after:" Anna eine Tasse Kaffee, bevor sie mit der Arbeit beginnt. ",
+    verb:"trinken",
+    answer:"trinkt",
+    signal:"jeden Morgen",
+    tense:"Präsens",
+    formation:"Verb im Präsens"
+},
 
-        {
-            before:"Im Moment ",
-            after:" Anna an einem wichtigen internationalen Projekt. ",
-            verb:"arbeiten",
-            answer:"arbeitet",
-            signal:"im Moment",
-            tense:"Präsens",
-            formation:"Verb im Präsens"
-        },
+{
+    before:"Im Moment ",
+    after:" Anna an einem wichtigen internationalen Projekt. ",
+    verb:"arbeiten",
+    answer:"arbeitet",
+    signal:"im Moment",
+    tense:"Präsens",
+    formation:"Verb im Präsens"
+},
 
-        {
-            before:"Gestern ",
-            after:" Anna schon sehr früh ins Büro. ",
-            verb:"fahren",
-            answer:"fuhr",
-            signal:"gestern",
-            tense:"Präteritum",
-            formation:"Präteritum"
-        },
+{
+    before:"Gestern ",
+    after:" Anna schon sehr früh ins Büro. ",
+    verb:"fahren",
+    answer:"fuhr",
+    signal:"gestern",
+    tense:"Präteritum",
+    formation:"Präteritum"
+},
 
-        {
-            before:"Dort ",
-            after:" sie ihren Chef vor dem Besprechungsraum. ",
-            verb:"treffen",
-            answer:"traf",
-            signal:"Erzählung in der Vergangenheit",
-            tense:"Präteritum",
-            formation:"Präteritum"
-        },
+{
+    before:"Dort ",
+    after:" sie ihren Chef vor dem Besprechungsraum. ",
+    verb:"treffen",
+    answer:"traf",
+    signal:"Erzählung in der Vergangenheit",
+    tense:"Präteritum",
+    formation:"Präteritum"
+},
 
-        {
-            before:"Anna ",
-            after:" bereits an mehreren internationalen Projekten gearbeitet. ",
-            verb:"haben",
-            answer:"hat",
-            signal:"bereits",
-            tense:"Perfekt",
-            formation:"haben + Partizip II"
-        },
+{
+    before:"Anna ",
+    after:" bereits an mehreren internationalen Projekten <span class='predicate'>gearbeitet</span>. ",
+    verb:"haben",
+    answer:"hat",
+    signal:"bereits",
+    tense:"Perfekt",
+    formation:"haben + Partizip II"
+},
 
-        {
-            before:"Außerdem ",
-            after:" sie schon mehrmals nach Spanien gereist. ",
-            verb:"sein",
-            answer:"ist",
-            signal:"schon mehrmals",
-            tense:"Perfekt",
-            formation:"sein + Partizip II"
-        },
+{
+    before:"Außerdem ",
+    after:" sie schon mehrmals nach Spanien <span class='predicate'>gereist</span>. ",
+    verb:"sein",
+    answer:"ist",
+    signal:"schon mehrmals",
+    tense:"Perfekt",
+    formation:"sein + Partizip II"
+},
 
-        {
-            before:"Bevor Anna gestern ins Büro kam, ",
-            after:" ihr Chef bereits eine wichtige E-Mail geschrieben. ",
-            verb:"haben",
-            answer:"hatte",
-            signal:"bevor",
-            tense:"Plusquamperfekt",
-            formation:"hatte + Partizip II"
-        },
+{
+    before:"Bevor Anna gestern ins Büro kam, ",
+    after:" ihr Chef bereits eine wichtige E-Mail <span class='predicate'>geschrieben</span>. ",
+    verb:"haben",
+    answer:"hatte",
+    signal:"bevor",
+    tense:"Plusquamperfekt",
+    formation:"hatte + Partizip II"
+},
 
-        {
-            before:"Anna war gut vorbereitet, weil sie alle Unterlagen vorher ",
-            after:". ",
-            verb:"lesen",
-            answer:"gelesen hatte",
-            signal:"vorher",
-            tense:"Plusquamperfekt",
-            formation:"Partizip II + hatte"
-        },
+{
+    before:"Anna war gut vorbereitet, weil sie alle Unterlagen vorher ",
+    after:".",
+    verb:"lesen",
+    answer:"gelesen hatte",
+    signal:"vorher",
+    tense:"Plusquamperfekt",
+    formation:"Partizip II + hatte"
+},
 
-        {
-            before:"Nächste Woche ",
-            after:" Anna nach Madrid reisen. ",
-            verb:"werden",
-            answer:"wird",
-            signal:"nächste Woche",
-            tense:"Futur I",
-            formation:"werden + Infinitiv"
-        },
+{
+    before:"Nächste Woche ",
+    after:" Anna nach Madrid <span class='predicate'>reisen</span>. ",
+    verb:"werden",
+    answer:"wird",
+    signal:"nächste Woche",
+    tense:"Futur I",
+    formation:"werden + Infinitiv"
+},
 
-        {
-            before:"Dort ",
-            after:" sie mehrere internationale Geschäftspartner treffen. ",
-            verb:"werden",
-            answer:"wird",
-            signal:"zukünftige Handlung",
-            tense:"Futur I",
-            formation:"werden + Infinitiv"
-        },
+{
+    before:"Dort ",
+    after:" sie mehrere internationale Geschäftspartner <span class='predicate'>treffen</span>. ",
+    verb:"werden",
+    answer:"wird",
+    signal:"zukünftige Handlung",
+    tense:"Futur I",
+    formation:"werden + Infinitiv"
+},
 
-        {
-            before:"Bis Ende der nächsten Woche ",
-            after:" Anna alle wichtigen Gespräche geführt haben. ",
-            verb:"werden",
-            answer:"wird",
-            signal:"bis Ende der nächsten Woche",
-            tense:"Futur II",
-            formation:"werden + Partizip II + haben"
-        },
+{
+    before:"Bis Ende der nächsten Woche ",
+    after:" Anna alle wichtigen Gespräche <span class='predicate'>geführt haben</span>. ",
+    verb:"werden",
+    answer:"wird",
+    signal:"bis Ende der nächsten Woche",
+    tense:"Futur II",
+    formation:"werden + Partizip II + haben"
+},
 
-        {
-            before:"Bis dahin ",
-            after:" sie auch den neuen Vertrag unterschrieben haben. ",
-            verb:"werden",
-            answer:"wird",
-            signal:"bis dahin",
-            tense:"Futur II",
-            formation:"werden + Partizip II + haben"
-        },
+{
+    before:"Bis dahin ",
+    after:" sie auch den neuen Vertrag <span class='predicate'>unterschrieben haben</span>. ",
+    verb:"werden",
+    answer:"wird",
+    signal:"bis dahin",
+    tense:"Futur II",
+    formation:"werden + Partizip II + haben"
+},
 
-        {
-            before:"Annas Chef sagt, sie ",
-            after:" für das neue Projekt besonders gut geeignet. ",
-            verb:"sein",
-            answer:"sei",
-            signal:"indirekte Rede",
-            tense:"Konjunktiv I",
-            formation:"Konjunktiv I"
-        },
+{
+    before:"Annas Chef sagt, sie ",
+    after:" für das neue Projekt besonders gut geeignet. ",
+    verb:"sein",
+    answer:"sei",
+    signal:"indirekte Rede",
+    tense:"Konjunktiv I",
+    formation:"Konjunktiv I"
+},
 
-        {
-            before:"Er erklärt außerdem, Anna ",
-            after:" bereits viel internationale Erfahrung gesammelt. ",
-            verb:"haben",
-            answer:"habe",
-            signal:"indirekte Rede",
-            tense:"Konjunktiv I Perfekt",
-            formation:"habe + Partizip II"
-        },
+{
+    before:"Er erklärt außerdem, Anna ",
+    after:" bereits viel internationale Erfahrung <span class='predicate'>gesammelt</span>. ",
+    verb:"haben",
+    answer:"habe",
+    signal:"indirekte Rede",
+    tense:"Konjunktiv I Perfekt",
+    formation:"habe + Partizip II"
+},
 
-        {
-            before:"Wenn Anna mehr Freizeit hätte, ",
-            after:" sie noch eine weitere Sprache lernen. ",
-            verb:"werden",
-            answer:"würde",
-            signal:"wenn",
-            tense:"Konjunktiv II",
-            formation:"würde + Infinitiv"
-        },
+{
+    before:"Wenn Anna mehr Freizeit hätte, ",
+    after:" sie noch eine weitere Sprache <span class='predicate'>lernen</span>. ",
+    verb:"werden",
+    answer:"würde",
+    signal:"wenn",
+    tense:"Konjunktiv II",
+    formation:"würde + Infinitiv"
+},
 
-        {
-            before:"Wenn sie früher von dem Projekt gewusst hätte, ",
-            after:" sie sich noch besser vorbereitet. ",
-            verb:"haben",
-            answer:"hätte",
-            signal:"wenn ... hätte",
-            tense:"Konjunktiv II Vergangenheit",
-            formation:"hätte + Partizip II"
-        },
+{
+    before:"Wenn sie früher von dem Projekt gewusst hätte, ",
+    after:" sie sich noch besser <span class='predicate'>vorbereitet</span>. ",
+    verb:"haben",
+    answer:"hätte",
+    signal:"wenn ... hätte",
+    tense:"Konjunktiv II Vergangenheit",
+    formation:"hätte + Partizip II"
+},
 
-        {
-            before:"Der neue Vertrag ",
-            after:" gestern von der Geschäftsleitung unterschrieben. ",
-            verb:"werden",
-            answer:"wurde",
-            signal:"von der Geschäftsleitung",
-            tense:"Präteritum Passiv",
-            formation:"wurde + Partizip II"
-        },
+{
+    before:"Der neue Vertrag ",
+    after:" gestern von der Geschäftsleitung <span class='predicate'>unterschrieben</span>. ",
+    verb:"werden",
+    answer:"wurde",
+    signal:"von der Geschäftsleitung",
+    tense:"Präteritum Passiv",
+    formation:"wurde + Partizip II"
+},
 
-        {
-            before:"Die Reise ",
-            after:" bereits von Annas Kollegin organisiert worden. ",
-            verb:"sein",
-            answer:"ist",
-            signal:"Passiv",
-            tense:"Perfekt Passiv",
-            formation:"ist + Partizip II + worden"
-        },
+{
+    before:"Die Reise ",
+    after:" bereits von Annas Kollegin <span class='predicate'>organisiert worden</span>. ",
+    verb:"sein",
+    answer:"ist",
+    signal:"Passiv",
+    tense:"Perfekt Passiv",
+    formation:"ist + Partizip II + worden"
+},
 
-        {
-            before:"Vor Annas Ankunft ",
-            after:" alle wichtigen Termine bereits vereinbart worden. ",
-            verb:"sein",
-            answer:"waren",
-            signal:"vor Annas Ankunft",
-            tense:"Plusquamperfekt Passiv",
-            formation:"waren + Partizip II + worden"
-        },
+{
+    before:"Vor Annas Ankunft ",
+    after:" alle wichtigen Termine bereits <span class='predicate'>vereinbart worden</span>. ",
+    verb:"sein",
+    answer:"waren",
+    signal:"vor Annas Ankunft",
+    tense:"Plusquamperfekt Passiv",
+    formation:"waren + Partizip II + worden"
+},
 
-        {
-            before:"Bis Freitag ",
-            after:" der endgültige Bericht fertiggestellt worden sein. ",
-            verb:"werden",
-            answer:"wird",
-            signal:"bis Freitag",
-            tense:"Futur II Passiv",
-            formation:"werden + Partizip II + worden sein"
-        }
+{
+    before:"Bis Freitag ",
+    after:" der endgültige Bericht <span class='predicate'>fertiggestellt worden sein</span>. ",
+    verb:"werden",
+    answer:"wird",
+    signal:"bis Freitag",
+    tense:"Futur II Passiv",
+    formation:"werden + Partizip II + worden sein"
+}
 
-    ]
+]
 
 };
+
